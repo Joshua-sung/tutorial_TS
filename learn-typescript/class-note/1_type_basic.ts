71d5eb10b97b752f815adf0ee6acd42881734e59
joshua <krjoshua21@gmail.com>
2022-01-01 23:15:12 +0900

    2nd day

--- learn-typescript/class-note/1_type_basic.ts
+++ learn-typescript/class-note/1_type_basic.ts
@@ -1,15 +1,18 @@
+//변수 지정하기
+
+//문자열로 지정
 const str: string  = 'hi';
-
+//숫자로지정
 let num: number = 10;
-
+//배열로지정
 let arr: Array<number> =[1,2,3];
 
 let heros : Array<string> = ['a','s','b'];
-
+//배열안에 숫자가 들어감을 지정
 let items: number[] = [1,2,3]
-
+//객체형태로 배열이 들어감을 지정
 let address: [string,number] = ['seoul',1];
-
+//객체라고 지정
 let obj: object = {};
 
 let a:boolean = true
@@ -30,3 +33,4 @@ function add():number{
 function hab(a:number, b:number):number{
     return a + b ;
 }
+
